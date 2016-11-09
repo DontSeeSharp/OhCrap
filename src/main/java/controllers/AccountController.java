bf9ff0af2ca09d5d731baff265bb19d11e28588a
Taavi.Sutt <taavi.sutt@ttu.ee>
2016-11-09 15:15:39 +0200

    Retrieve userdata form database

--- src/main/java/controllers/AccountController.java
+++ src/main/java/controllers/AccountController.java
@@ -1,8 +1,9 @@
 package controllers;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-import dto.save.CreateAccountRequest;
+import domain.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Taavi on 30.10.2016.
@@ -30,12 +34,27 @@ public class AccountController {
         this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
+    @RequestMapping(value = "getUsers")
+    public List<Account> getUsers() {
+        return jdbcTemplate.query("select * from userdata", new AccountMapper());
+    }
+
     @RequestMapping(value = "createUser", method = RequestMethod.POST)
-    public void createUser(@RequestBody CreateAccountRequest request) {
+    public void createUser(@RequestBody Account request) {
         HashMap hashMap = new HashMap();
         hashMap.put("username", request.getUsername());
         hashMap.put("password", request.getPassword());
         jdbcTemplate.update("insert into userdata (username, password) VALUES(:username, :password)", hashMap);
     }
 
+    private static class AccountMapper implements RowMapper<Account> {
+        public Account mapRow(ResultSet res, int rowNum) throws SQLException {
+            Account account = new Account();
+            account.setId(res.getInt("id"));
+            account.setUsername(res.getString("username"));
+            account.setPassword(res.getString("password"));
+            return account;
+        }
+
+    }
 }
--- src/main/java/domain/Account.java
+++ src/main/java/domain/Account.java
@@ -1,14 +1,21 @@
-package dto.save;
+package domain;
 
 /**
  * Created by Taavi on 30.10.2016.
  */
-public class CreateAccountRequest {
+public class Account {
 
+    private int id;
     private String username;
     private String password;
 
+    public void setId(int id) {
+        this.id = id;
 
+    }
+    public int getId() {
+        return id;
+    }
     public String getPassword() {
         return password;
     }
