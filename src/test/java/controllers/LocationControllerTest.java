51c3210b36e96309073852070e58356ea0a240a5
Taavi.Sutt <taavi.sutt@ttu.ee>
2016-12-21 12:17:08 +0200

    Fix for popup typo

--- src/test/java/controllers/LocationControllerTest.java
+++ src/test/java/controllers/LocationControllerTest.java
@@ -9,26 +9,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
-<<<<<<< Updated upstream
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collections;
-=======
-import org.springframework.security.test.context.support.WithMockUser;
-
->>>>>>> Stashed changes
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+//<<<<<<< Updated upstream
+//=======
+//>>>>>>> Stashed changes
 
 /**
  * Created by Hendrig on 20.12.2016.
